987a7b8e0872b6cfa8727a327ba19ecccda2a9e1
dartmonkeyExeter <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-10-01 01:58:02 +0100

    multiple doctors
    it took ages but you can now have multiple doctors (classes are hard but really dope)

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # initialize empty queues and doctor lists
-priorityQueue = []
-normalQueue = []
-priorityDoctor = []
-normalDoctor = []
+
 doctors = []
 
 class Doctor:
@@ -10,37 +7,51 @@ class Doctor:
         self.queue = queue
         self.name = name
 
-while True:
-    num_doctors = input('how many doctors are here today?\n')
-    try:
-        num_doctors = int(num_doctors)
+def doctor_define(doctors):
+    doctors = []
+    while True:
+        num_doctors = input('how many doctors are here today?\n')
+        try:
+            num_doctors = int(num_doctors)
+        except:
+            print('please insert an integer value.')
+            continue
+        if num_doctors <= 0:
+            print("there must be at least 1 doctor, please try again")
+            continue
         break
-    except:
-        print('please insert an integer value.')
-        continue
 
-for i in range(num_doctors):
-    name = input(f"enter the name of doctor {i + 1}: ")
-    queue = []
-    doctors.append(Doctor(name, queue))
+    for i in range(num_doctors):
+        name = input(f"enter the name of doctor {i + 1}: ")
+        queue = []
+        doctors.append(Doctor(name, queue))
 
-for i, doctor in enumerate(doctors):
-    print(f"Doctor {i + 1}: {doctor.name}")
-
-
+    for i, doctor in enumerate(doctors):
+        print(f"Doctor {i + 1}: {doctor.name}")
 
 
 # function to call the next patient
 def call_next_patient(doctors):
     while True:
-        whichDoctor = input('which doctors queue, please input as interger')
+        whosQueue = (input('whos queue should be called from? (as a number)\n'))
         try:
-            whichDoctor = int(whichDoctor)
+            whosQueue = int(whosQueue)
         except:
             print('write a number')
             continue
-        print(f'the next patient in doctor {doctors[whichDoctor - 1].name}s queue is {doctors[whichDoctor - 1].queue[0]}')
-        doctors[whichDoctor - 1].queue.pop(0)
+        if whosQueue <= 0:
+            print('number too small') 
+            continue
+        if not doctors[whosQueue - 1].queue:
+            print('that doctors queue is empty')
+            continue 
+        if whosQueue - 1 > len(doctors):
+            print('number too big')
+            continue
+        else:
+            print(f'the next patient in doctor {doctors[whosQueue - 1].name}s queue is {doctors[whosQueue - 1].queue[0]}')
+            doctors[whosQueue - 1].queue.pop(0)
+            break
 
 
 # function to add a patient to a queue
@@ -54,71 +65,41 @@ def add_patient(doctors):
         except:
             print('write a number')
             continue
-        if whosQueue > len(doctors[whosQueue - 1].queue) and whosQueue != len(doctors[whosQueue - 1].queue):
+        if whosQueue <= 0:
+            print('number too small') 
+            continue
+        if whosQueue - 1 > len(doctors):
             print('number too big')
             continue
-        doctors[whosQueue - 1].queue.append(nameToAdd)
-        print(f'added {nameToAdd} to the doctors queue')
+        else:
+            print(f'added {nameToAdd} to {doctors[whosQueue - 1].name}s queue')
+            doctors[whosQueue - 1].queue.append(nameToAdd)
+            break
 
 
-# function to remove a patient from a queue
-def remove_patient():
-    # join all names in each queue with a comma and space for printing purposes
-    s = ', '
-    p = s.join(priorityQueue)
-    n = s.join(normalQueue)
-
-    # ask which patient to remove
-    whom = str(input(f'who to remove\npriority: {p}\nnormal: {n}\n'))
-
-    count = 0
-
-    # if the patient is in the priority queue, remove them from both queues and their respective doctor lists
-    if whom in priorityQueue:
-        for i in priorityQueue:
-            count += 1
-            if i == whom:
-                priorityDoctor.pop(count - 1)
-                priorityQueue.pop(count - 1)
-
-    count = 0
-
-    # if the patient is in the normal queue, remove them from both queues and their respective doctor lists
-    if whom in normalQueue:
-        for i in normalQueue:
-            count += 1
-            if i == whom:
-                normalDoctor.pop(count - 1)
-                normalQueue.pop(count - 1)
-
-    print('if the patient has been found, they have been removed from the list.')
-
-
-def main(priorityQueue, normalQueue, priorityDoctor, normalDoctor):  # main function
-
-
-
+def main():  # main function
+    doctor_define(doctors)
     while True:  # loop for input protection
-
-        callOrQueue = str(input('do you want to (c)all the next patient, (a)dd a patient, (r)emove a patient? type '
-                                'clear to clear both queues.\n'))
+        callOrQueue = str(input('do you want to (c)all the next patient, (a)dd a patient, or (r)edfine the doctor list\n'))
         # calling functions based on input
         if callOrQueue == 'c':
-            call_next_patient(doctors)
+            all_queues_empty = True
+            for i in doctors:
+                if i.queue:
+                    all_queues_empty = False
+                    break
+            if all_queues_empty:
+                print("all queues are empty")
+            else:
+                call_next_patient(doctors)
         elif callOrQueue == 'a':
             add_patient(doctors)
         elif callOrQueue == 'r':
-            remove_patient()
-        elif callOrQueue == 'clear':
-            priorityQueue = []
-            normalQueue = []
-            priorityDoctor = []
-            normalDoctor = []
-            print('queues cleared')
+            doctor_define(doctors)
         else:  # input protection
             print('invalid input')
 
 
 if __name__ == '__main__':  # if the file name is main, run the file.
 
-    main(priorityQueue, normalQueue, priorityDoctor, normalDoctor)
+    main()
